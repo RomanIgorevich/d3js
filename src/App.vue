38d518b0ff04e688733f967da08e447afa574b9c
Roman Igorevich <roman.igorevich.ru@gmail.com>
2021-11-11 11:25:10 +0300

    графика на html

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
-  <div id="app">
-    <button @click="getIssues">click</button>
-    <!-- <chart :issues="issues" /> -->
-    <hr/>
-    <test :issues="issues" />
+  <div>
+    <button @click="goColor">go color</button>
+
+    <h2 class="h">Lorem, ipsum dolor.</h2>
+
+    <p>Lorem, ipsum dolor.</p>
+    <p>Lorem ipsum dolor sit amet.</p>
+    <p>Lorem, ipsum dolor.</p>
+    <!-- <button @click="drawingGraph">Отрисовка графика</button> -->
+    <div class="chartHtml"></div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
-
-// import Chart from "./components/Chart.vue";
-import Test from "./components/Test.vue";
+import * as d3 from "d3";
 
 export default {
   name: "app",
-  components: {
-    // Chart,
-    Test,
+  mounted() {
+    this.drawingGraph();
   },
   data() {
     return {
@@ -31,6 +33,46 @@ export default {
         this.issues = response.data;
       });
     },
+    goColor() {
+      d3.select(".h").style("color", "red");
+      d3.selectAll("p").style("color", function () {
+        return "hsl(" + Math.random() * 360 + ",100%, 50%)";
+      });
+      d3.selectAll("p")
+        .data([16, 24, 32])
+        .style("font-size", function (d) {
+          return d + "px";
+        });
+    },
+    drawingGraph() {
+      const data = [8, 10, 16, 14, 28, 8];
+      let x = d3.scaleLinear()
+        .domain([0, d3.max(data)])
+        .range([0, 350]);
+
+      d3.select(".chartHtml")
+        .selectAll("div")
+        .data(data)
+        .enter()
+        .append("div")
+        .style("width", function (d) {
+          return x(d) + "px";
+        })
+        .text(function (d) {
+          return d;
+        });
+    },
   },
 };
 </script>
+
+<style>
+.chartHtml div {
+  font: 10px sans-serif;
+  background: blue;
+  text-align: center;
+  padding: 3px;
+  margin: 1px;
+  color: white;
+}
+</style>
