2e1cf1662fca820f612e4a8781795df8a126ccba
Roman Igorevich <roman.igorevich.ru@gmail.com>
2021-11-11 09:04:11 +0300

    работа

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div id="app">
     <button @click="getIssues">click</button>
-    <chart :issues="issues" />
+    <!-- <chart :issues="issues" /> -->
+    <hr/>
+    <test :issues="issues" />
   </div>
 </template>
 
 <script>
 import axios from "axios";
 
-import Chart from "./components/Chart.vue";
+// import Chart from "./components/Chart.vue";
+import Test from "./components/Test.vue";
 
 export default {
   name: "app",
   components: {
-    Chart,
+    // Chart,
+    Test,
   },
   data() {
     return {
